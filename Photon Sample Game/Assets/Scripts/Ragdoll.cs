58fa8cd914dd0561f4bacee78c9baaa2a7381336
styrbo <styrbogoogle@mail.ru>
2019-12-17 22:46:17 +0500

    added Low Poly FPS Pack

---
+++ Photon Sample Game/Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    [SerializeField] private Rigidbody[] _rbs;
+
+    private void Awake()
+    {
+        SetRagdoll = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            SetRagdoll = true;
+    }
+
+    public bool SetRagdoll
+    {
+        set
+        {
+            var toConstrains = value ? RigidbodyConstraints.None : RigidbodyConstraints.FreezeAll;
+
+            foreach (var item in _rbs)
+            {
+                item.constraints = toConstrains;
+            }
+        }
+    }
+
+}
