4ac3eb04f792ee6a93a59046c887761bb376293c
styrbo <styrbogoogle@mail.ru>
2019-12-18 23:06:33 +0500

    added Pun2 Free

---
+++ Photon Sample Game/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private InputField _playerNickNameField;
+
+    private void Awake()
+    {
+        _playerNickNameField.onValueChanged.AddListener(OnPlayernameChanged);
+    }
+
+    public void Connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    private void OnPlayernameChanged(string value)
+    {
+        PhotonNetwork.NickName = value;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        RoomOptions options = new RoomOptions();
+
+        
+    }
+
+}
