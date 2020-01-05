3b6fe2458b876500068a96c235ad8ebf44a143c8
styrbo <styrbogoogle@mail.ru>
2020-01-05 12:55:50 +0500

    Revert "made main menu"
    This reverts commit 4c37f6ade7b166d391aec94b5289c2406f7a5a6b.

--- Photon Sample Game/Assets/Scripts/UI/MainMenu/RoomCreator.cs
+++ Photon Sample Game/Assets/Scripts/UI/MainMenu/RoomCreator.cs
@@ -3,10 +3,10 @@ using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RoomCreator : MonoBehaviourPunCallbacks
+public class RoomCreator : MonoBehaviour
 {
     [SerializeField] private GameObject _createPanel;
-    [SerializeField] private GameObject _roomLobby;
+
     [SerializeField] private Text _maxPlayerText;
 
     private const char InfinityChar = '∞';
@@ -35,14 +35,10 @@ public class RoomCreator : MonoBehaviourPunCallbacks
         _roomName = field.text;
     }
 
-    public override void OnCreatedRoom()
-    {
-        _roomLobby.SetActive(true);
-    }
-
     public void Create()
     {
         PhotonNetwork.CreateRoom(_roomName,_options);
+        Close();
     }
 
     public void Close()
--- Photon Sample Game/Assets/Scripts/UI/MainMenu/RoomLobbyManager.cs
+++ Photon Sample Game/Assets/Scripts/UI/MainMenu/RoomLobbyManager.cs
@@ -1,43 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using Photon.Pun;
-using Photon.Realtime;
 
-public class RoomLobbyManager : MonoBehaviourPunCallbacks
+public class RoomLobbyManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _content;
 
-    private Room _room;
-
-    private new void OnEnable()
+    // Use this for initialization
+    void Start()
     {
-        _room = PhotonNetwork.CurrentRoom;
+
     }
 
-    public override void OnPlayerEnteredRoom(Player newPlayer)
+    // Update is called once per frame
+    void Update()
     {
-        base.OnPlayerEnteredRoom(newPlayer);
-    }
 
-    public override void OnPlayerLeftRoom(Player otherPlayer)
-    {
-        base.OnPlayerLeftRoom(otherPlayer);
-    }
-
-    public override void OnLeftRoom()
-    {
-        Quit();
-    }
-
-    public void JoinToGame()
-    {
-        SceneLoader.LoadScene(1);
-    }
-
-    public void Quit()
-    {
-        PhotonNetwork.LeaveRoom();
-        gameObject.SetActive(false);
     }
 }
